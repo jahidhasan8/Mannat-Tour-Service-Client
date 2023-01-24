9c18eb8e2787850ed64548bcdd29ad95f694f03f
jahidhasan8 <jahidhasan1932@gmail.com>
2023-01-24 19:03:00 +0600

    Footer modified

--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -1,55 +1,63 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import { AiOutlineMail } from "react-icons/ai";
-import { IoCallOutline } from "react-icons/io5";
-import { BsFacebook,BsInstagram,BsTwitter } from "react-icons/bs";
+import './Footer.css';
+import footerBg from '../../../images/footer-bg.png';
+
 const Footer = () => {
-    return (
-
-        <div fixed="bottom" className="mt-5 ">  
-
-            <footer className="text-center text-white bg-dark mx-0 row">
-
-                <div className="col-md-4  mt-4">
-                    <h6 className=" mb-4 font-weight-bold">Contact</h6>
-                    <p> </p>
-                    <p className='m4-3'><AiOutlineMail></AiOutlineMail>  jahid@gmail.com</p>
-                    <p className='m4-3'><IoCallOutline></IoCallOutline> +8801929520000</p>
-                </div>
-
-                <section className='col-md-4 '>
-                    <div className="container p-4 pb-0">
-
-                        <section className="">
-                            <p className="d-flex justify-content-center align-items-center">
-                                <span className="me-3">Register for free</span>
-                                <Link to='/register'><button type="button" className="btn btn-outline-light btn-rounded fw-bold">
-                                    Register!
-                                </button></Link>
-                            </p>
-                        </section>
-
+     return (
+          <div style={{ backgroundImage: `url(${footerBg})`, height:"100vh", backgroundPosition: "center center", paddingTop: "160px", backgroundSize : "cover"}}>
+              
+              <div className="container">
+                    <div className="row bottom-footer-wrapper">
+                         <div className="col-lg-3 col-md-6 col-12">
+                            <div className="footer-logo">
+                                 
+                                 <h3>Mannat Tour Service</h3>
+                            </div>  
+                            <div>
+                                 <p className="contact-link"><i className="fas fa-phone"></i> +1 (009) 99 143 77</p>
+                                 <p className="contact-link"><i className="far fa-envelope"></i> travelcontact@agency.mail</p>
+                                 <p className="contact-link"><i className="fas fa-map-marker-alt"></i> Zip : +990 hilTown,  </p>
+                            </div>
+                         </div>
+                         <div className="col-lg-3 col-md-6 col-12">
+                              <h3 className="bottom-title">Quick Links</h3>
+                              <p className="connect-link"><i className="fas fa-arrow-right"></i> Famous Tour</p>
+                              <p className="connect-link"><i className="fas fa-arrow-right"></i> Tour Blogs</p>
+                              <p className="connect-link"><i className="fas fa-arrow-right"></i>Find Place</p>
+                              <p className="connect-link"><i className="fas fa-arrow-right"></i>
+                              Take Ticket</p>
+                              <p className="connect-link"><i className="fas fa-arrow-right"></i>
+                              Winter Place</p>
+                         </div>
+                         <div className="col-lg-3 col-md-6 col-12">
+                              <h3 className="bottom-title">Important Links</h3>
+                              <p className="connect-link"><i className="fas fa-arrow-right"></i>
+                              Destination</p>
+                              <p className="connect-link"><i className="fas fa-arrow-right"></i>
+                              Get In Touch</p>
+                              <p className="connect-link"><i className="fas fa-arrow-right"></i>
+                              Who We Are</p>
+                              <p className="connect-link"><i className="fas fa-arrow-right"></i>
+                              Contact Us</p>
+                         </div>
+                         <div className="col-lg-3 col-md-6 col-12">
+                              <div className="input-bottom">
+                                   <input placeholder="enter your email" type="email" name="" id="" />
+                                   <input placeholder="your report" type="text" name="" id="" />
+                                   
+                                   <input className="submitBtn" type="submit" value="Send Mail" />
+                              </div>
+                         </div>
                     </div>
-                    <div className="text-center p-3" >
-                        © 2025 Copyright: Mannat Tour Service
+              </div>
 
-                    </div>
-                </section>
-
-                <div className="col-md-4  mt-4">
-                    <h6 className=" mb-4 font-weight-bold">Follow Me</h6>
-                    <Link className='fs-5'><BsFacebook></BsFacebook></Link>
-                    <Link  className='ms-3 fs-5'><BsInstagram></BsInstagram></Link>
-                    <Link className='ms-3 fs-5'><BsTwitter></BsTwitter></Link>
-                    
+              <div className="mini-footer">
+                <div className="copy-footer">
+                     <p>© CopyRight Privacy | Policy  2023</p>
                 </div>
-
-            </footer>
-
-
-        </div>
-
-    );
+              </div>
+          </div>
+     );
 };
 
 export default Footer;
